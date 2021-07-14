59b27d37cae4bb3a0fd9e5d76451925dcc57e11e
Tim <a.reflesh@gmail.com>
2021-07-14 09:49:24 -0400

    tracking

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from plyer import notification
 import psycopg2
 from Levenshtein import distance
 from nltk.translate.bleu_score import sentence_bleu
+import datetime 
 st.set_page_config(layout="wide")
 @st.cache
 def get_file_list():
@@ -14,13 +15,13 @@ def get_file_list():
     content = my_file.read()
     fl = content.split("\n")    
     return fl
-state = SessionState.get(n = 0)
+state = SessionState.get(n = 0,start_time = datetime.datetime.now().time().strftime('%H:%M:%S'))
 name = st.sidebar.text_input("Input your name and press Enter please:","")
 DATABASE_URL = os.environ['DATABASE_URL']
 if (name!=''):
     con = psycopg2.connect(DATABASE_URL)
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS annotations (id serial PRIMARY KEY, name varchar, file varchar, annotation text);")
+    cur.execute("CREATE TABLE IF NOT EXISTS annotations (id serial PRIMARY KEY, name varchar, file varchar, annotation text, track_time timestamp);")
     #st.sidebar.markdown("** Attention! ** Before closing the app please close connection with database")
     #work_dir = "./paintings/"+name+"/"
     #if not os.path.exists(work_dir):
@@ -56,22 +57,24 @@ if (name!=''):
     anno_place = col2.empty()
     value = " "
     annotation = anno_place.text_area("Input annotation:", value, height=100, key = 1)
-    if annotation:
-        if meta_data["annot"]!="nan":
-            col2.markdown(" ** BLEU Score: **"+str(sentence_bleu([meta_data["annot"].split(" ")],annotation.split(" "))))
-            col2.markdown(" ** Levenshtein distance: **"+str(distance(meta_data["annot"],annotation)))
+    #if annotation:
+        #if meta_data["annot"]!="nan":
+        #    col2.markdown(" ** BLEU Score: **"+str(sentence_bleu([meta_data["annot"].split(" ")],annotation.split(" "))))
+        #    col2.markdown(" ** Levenshtein distance: **"+str(distance(meta_data["annot"],annotation)))
     if col2.button("I like it! Save! "):   
         print(image_name)
         print(annotation)
         value = " "
-        
+        end_time = datetime.datetime.now().time().strftime('%H:%M:%S')
+        total_time=(datetime.datetime.strptime(end_time,'%H:%M:%S') - datetime.datetime.strptime(state.start_time,'%H:%M:%S'))
         annot = {"File":image_name}
         annot[name]=annotation
-        cur.execute("INSERT INTO annotations (name, file, annotation) VALUES (%s, %s, %s)",(name, image_name, annotation))
+        cur.execute("INSERT INTO annotations (name, file, annotation, track_time) VALUES (%s, %s, %s)",(name, image_name, annotation,total_time))
         con.commit()
         #with open(work_dir+os.path.splitext(image_name)[0]+'.json', 'w') as json_file:
             #json.dump(annot, json_file)
-        
+        state.start_time = datetime.datetime.now().time().strftime('%H:%M:%S')
+
     if col2.button("Next image",key = state.n):
         state.n=state.n+1
         if state.n == 101:
@@ -85,12 +88,12 @@ if (name!=''):
     #if st.sidebar.button("Close connection"):
         
         #st.sidebar.write("Connection is closed")
-    col2.markdown('''<p style='text-align: justify;'>The BLEU score compares a sentence against one or more reference sentences and tells how well does the 
-                    candidate sentence matched the list of reference sentences. It gives an output score between 0 and 1. A BLEU score of 1 means that the 
-                    candidate sentence perfectly matches one of the reference sentences. <br><br>
-                    The distance value describes the minimal number of deletions, insertions, or substitutions that are required to transform one string (the source) 
-                    into another (the target).The greater the Levenshtein distance, the greater is the difference between the strings. 
-                    </p>''',unsafe_allow_html=True)
+    #col2.markdown('''<p style='text-align: justify;'>The BLEU score compares a sentence against one or more reference sentences and tells how well does the 
+    #                candidate sentence matched the list of reference sentences. It gives an output score between 0 and 1. A BLEU score of 1 means that the 
+    #                candidate sentence perfectly matches one of the reference sentences. <br><br>
+    #                The distance value describes the minimal number of deletions, insertions, or substitutions that are required to transform one string (the source) 
+    #                into another (the target).The greater the Levenshtein distance, the greater is the difference between the strings. 
+    #                </p>''',unsafe_allow_html=True)
     con.commit()
     cur.close()
     con.close()
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import datetime
+import SessionState
 st.set_page_config(layout="wide")
-col1,col2 = st.beta_columns(2)
-place = col2.empty()
-col1.text("sign")
-place.text("sign")
+state = SessionState.get(start_time = datetime.datetime.now().time().strftime('%H:%M:%S'))
+if st.button("Show time"):
+    end_time = datetime.datetime.now().time().strftime('%H:%M:%S')
+    total_time=(datetime.datetime.strptime(end_time,'%H:%M:%S') - datetime.datetime.strptime(state.start_time,'%H:%M:%S'))
+    st.write(state.start_time)
+    st.write(end_time)
+    st.write(total_time)
+    state.start_time = datetime.datetime.now().time().strftime('%H:%M:%S')
